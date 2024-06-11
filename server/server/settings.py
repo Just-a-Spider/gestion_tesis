dff1df140a86163982c2798e937ecf460ee6e87c
Just-a-Spider <andre_arg_0116@outlook.com>
2024-06-11 00:19:50 -0500

    Ahora se usa SQLite3 en vez de Postgres

--- server/server/settings.py
+++ server/server/settings.py
@@ -100,12 +100,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'postgres',
-        'PASSWORD': '0116',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
