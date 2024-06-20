f09d273dd3444183cea44d95202a5f9f2542a6c0
Just-a-Spider <andre_arg_0116@outlook.com>
2024-06-20 10:27:47 -0500

    Refactor admin.py to register new user models

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import *
 
-admin.site.register(Usuario)
+admin.site.register(Tesista)
+admin.site.register(Profesor)
+admin.site.register(Coordinador)
+admin.site.register(SecretariaCoord)
+admin.site.register(Decana)
+admin.site.register(SecretariaFacultad)
